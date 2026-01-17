e1a8b422a2e02823081e8aeba17836ae567965a1
iconsumeplutonium <uzymc242@outlook.com>
2026-01-16 23:12:06 -0800

    openapi spec

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,10 +1,31 @@
 import express from "express";
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
+import cors from 'cors';
+import * as OpenApiValidator from 'express-openapi-validator';
 import * as pinRoutes from './routes/pins.ts';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const apiSpec = path.join(__dirname, './openapi.yml');
+
+app.use(cors(
+    {origin: 'http://localhost:3000'},
+    {origin: 'http://localhost:4173'},
+    {origin: 'http://localhost:5173'},
+));
+
+
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: apiSpec,
+      validateRequests: true,
+      validateResponses: true,
+    }),
+);	
 
 app.get("/heartbeat", (req, res) => {
   console.log("[Server-side] Server received conn");
@@ -14,12 +35,10 @@ app.get("/heartbeat", (req, res) => {
   });
 });
 
-
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Express!" });
-});
 app.get("/api/pins", pinRoutes.getAllPins);
 app.get("/api/pins/:id", pinRoutes.getPin);
+app.post("/api/pins", pinRoutes.createPin);
+app.put("/api/pins", pinRoutes.editPin);
 
 app.listen(PORT, () => {
   console.log(`Backend listening on http://localhost:${PORT}`);
--- Backend/routes/pins.ts
+++ Backend/routes/pins.ts
@@ -11,3 +11,27 @@ export function getPin(req: Request, res: Response) {
 	const results = db.query(`SELECT * FROM pin WHERE id = ?`, [pinID]);
 	res.json(results);
 }
+
+export function createPin(req: Request, res: Response) {
+	// if (!req.body) {
+	// 	res.status(400).send();
+	// 	return;
+	// }	
+
+	const results = db.query(`
+		INSERT INTO pin(creatorID, message, image, color)
+		VALUES(?, ?, ?, ?)
+		RETURNING id;
+	`, [
+		req.body.creatorID ?? 1, 
+		req.body.message ?? null,
+		req.body.image ?? null,
+		req.body.color ?? null
+	]);
+
+	res.json(results[0]);
+}
+
+export function editPin(req: Request, res: Response) {
+
+}
