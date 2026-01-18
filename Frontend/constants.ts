28a64819ed31af642d761cf383af664419cbe57f
Prasiddh Pooskur <61389919+prapooskur@users.noreply.github.com>
2026-01-17 19:19:45 -0800

    darken a bit

--- Frontend/constants.ts
+++ Frontend/constants.ts
@@ -5,7 +5,7 @@ const API_URL_PROD = "https://api.truthnuke.tech";
 
 export const BASE_API_URL = import.meta.env.VITE_API_URL == "local" ? API_URL_DEV : API_URL_PROD;
 export const PIN_COLOR = "#007CBF";
-export const USER_PIN_COLOR = "#FFD700";
+export const USER_PIN_COLOR = "#FFC700";
 
 export const PIN_LAYER_STYLE: CircleLayerSpecification = {
 	id: "point",
